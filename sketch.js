92cd71b44d4cb4b79f411c11722a6bf4692197c0
taohsu <ixmlmxi@gmail.com>
2019-02-16 09:42:44 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,11 +13,17 @@ function setup() {
   
   
   var news = data.feeds;
+  var subtitle = select('#subtitle');
+  subtitle.html(data.description);
+  
   for (var i = 0; i < news.length; i ++) {
   	// createElement('h2', news[i].title);
-    var div0 = createDiv();
+    var card = createDiv();
     var link = news[i].link;
     console.log(link);
+    
+    
+    
     title = createA(news[i].link, news[i].title, '_blank');
     var author = createElement('p', '😉' + news[i].author);
     var date = createElement('p', news[i].date);
@@ -26,17 +32,17 @@ function setup() {
         // createElement('br');
     // author.
       var frame = select('#001');
-  frame.child(div0);
-    type.parent(div0);
-    title.parent(div0);
+  frame.child(card);
+    type.parent(card);
+    title.parent(card);
     title.class('h5');
     // console.log(title);
-    author.parent(div0);
-    date.parent(div0);
+    author.parent(card);
+    date.parent(card);
     date.class('small');
     
     // dvd.parent(div0);
-    div0.class('card mb-3 p-4');
+    card.class('card mb-3 p-4');
   }
   
 
